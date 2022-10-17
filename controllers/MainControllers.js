ea922cc553cba3d3209b6c4743371aadd55d4859
Akshay-gondhale <55553583+Akshay-gondhale@users.noreply.github.com>
2022-10-17 16:37:32 +0530

    new apis added for customer and rent

--- controllers/MainControllers.js
+++ controllers/MainControllers.js
@@ -1,27 +1,24 @@
 const axios = require("axios");
+const { bloqitBaseUrl } = require("../utils/Baseurls");
 const ProcessData = async (req, res) => {
   try {
     console.log("---------------- got request --------------------------");
     console.log(req.body);
     console.log("---------------- got request --------------------------");
-    axios
-      .post(
-        "https://script.google.com/macros/s/AKfycbzO4pL6HQLCVgg1v3fBWFPiOBJ8f9NjGzzQNWKgCjacilky_5N_XSNSbR1IBrh6p1eu/exec?gid=1774646088",
-        req.body
-      )
-      .then((res) => {
-        console.log(
-          "--------------------------- data added in sheet ---------------------------"
-        );
-        console.log(res.data);
-        console.log(
-          "--------------------------- data added in sheet ---------------------------"
-        );
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log(err.response);
-      });
+    const data = req.body;
+    const gettingCustomerDetails = await axios.get(
+      `${bloqitBaseUrl}/customers/${data.customer}`
+    );
+    // data.customer = gettingCustomerDetails;
+    const gettingRentDetails = await axios.get(
+      `${bloqitBaseUrl}/rents/${data.rent}`
+    );
+    console.log({ gettingCustomerDetails, gettingRentDetails });
+    // data.rent = gettingRentDetails;
+    const googleSheetResponse = await axios.post(
+      "https://script.google.com/macros/s/AKfycbzO4pL6HQLCVgg1v3fBWFPiOBJ8f9NjGzzQNWKgCjacilky_5N_XSNSbR1IBrh6p1eu/exec?gid=1774646088",
+      req.data
+    );
 
     res.status(200).json({
       msg: "Data Added",
